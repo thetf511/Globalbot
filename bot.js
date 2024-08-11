2147f899897277f26188a6a3d70273b55350be78
TheTF <89793305+thetf511@users.noreply.github.com>
2024-08-11 18:34:06 +0200

    Update bot.js
    Bug fix

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const cooldownTime = 5000;
 
 client.once('ready', () => {
   console.log(`Eingeloggt als ${client.user.tag}`);
+
   updateStatus();
 
   setInterval(() => {
@@ -24,51 +25,111 @@ function updateStatus() {
   client.user.setActivity(randomActivity.name, { type: randomActivity.type });
 }
 
+const blockedUsers = new Map();
+
 client.on('messageCreate', async message => {
-  if (message.content.startsWith(`${prefix}block`)) {
+  if (message.content.startsWith('!block')) {
     const args = message.content.split(' ');
     if (args.length >= 3) {
-      const targetUser = message.mentions.users.first();
-      if (targetUser) {
-        const reason = args.slice(2).join(' ');
-        message.reply(`Du hast ${targetUser.tag} erfolgreich blockiert. Grund: ${reason}`);
-      } else {
-        message.reply('Du musst einen Benutzer mit "@" erwähnen.');
-      }
+      const userID = args[1];
+      const reason = args.slice(2).join(' ');
+      blockedUsers.set(userID, reason);
+      message.channel.send(`Der Benutzer mit der ID ${userID} wurde erfolgreich blockiert. Grund: ${reason}`);
     } else {
-      message.reply('Falsche Verwendung des Befehls. Verwende: !block @username grund');
+      message.channel.send('Falsche Verwendung des Befehls. Verwende: !block <UserID> <Grund>');
     }
-  } else if (!blockedUsers.has(message.author.id)) {
+  }
+
+  if (message.content === '!blocklist') {
+    if (blockedUsers.size > 0) {
+      const embed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Liste der blockierten Benutzer');
+      
+      blockedUsers.forEach((reason, userID) => {
+        embed.addField(`UserID: ${userID}`, `Grund: ${reason}`);
+      });
+
+      message.channel.send({ embeds: [embed] });
+    } else {
+      message.channel.send('Keine blockierten Benutzer gefunden.');
+    }
+  }
+
+  if (blockedUsers.has(message.author.id)) {
+    message.delete();
+    return;
+  }
+
+  if (!message.author.bot && message.channel.name && message.channel.name.toLowerCase().includes('global')) {
+    if (message.content.trim() !== '' || message.attachments.size > 0 || message.stickers.size > 0) {
+      const cleanedContent = message.cleanContent; 
+      const combinedMessage = `${cleanedContent}\n[Invite me](https://discord.com/api/oauth2/authorize?client_id=1141889300330455080&permissions=8&scope=bot)・[Support](https://discord.gg/5cCAVMV6Me)`;
+
+      const embed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setAuthor(`${message.guild.name} - ${message.author.tag}`, message.guild.iconURL())
+        .setDescription(combinedMessage)
+        .setFooter(`User ID: ${message.author.id}`);
+
+      if (message.attachments.size > 0) {
+        const attachment = message.attachments.first();
+        embed.setImage(attachment.url);
+      }
+
+      if (message.stickers.size > 0) {
+        const sticker = message.stickers.first();
+        embed.setImage(sticker.url);
+      }
+
+      const globalChatChannel = message.guild.channels.cache.find(channel => channel.name.toLowerCase().includes('global'));
+      if (globalChatChannel) {
+        const sentMessage = await globalChatChannel.send({ embeds: [embed] });
+
+        message.delete().catch(console.error);
+      }
+    }
+  }
+
+  if (!blockedUsers.has(message.author.id)) {
     if (message.channel.name && message.channel.name.includes('global') && !message.author.bot) {
       if (!cooldown.has(message.author.id)) {
-        if (message.content.includes('@')) {
-          await message.delete().catch(console.error);
-        } else {
-          cooldown.add(message.author.id);
+        cooldown.add(message.author.id);
 
-          setTimeout(() => {
-            cooldown.delete(message.author.id);
-          }, cooldownTime);
+        setTimeout(() => {
+          cooldown.delete(message.author.id);
+        }, cooldownTime);
 
-          client.guilds.cache.forEach(guild => {
-            if (guild.id !== message.guild.id) {
-              const globalChatChannel = guild.channels.cache.find(
-                channel => channel.name && channel.name.includes('global') && channel.type === 'GUILD_TEXT'
-              );
+        client.guilds.cache.forEach(guild => {
+          if (guild.id !== message.guild.id) {
+            const globalChatChannel = guild.channels.cache.find(
+              channel => channel.name && channel.name.includes('global') && channel.type === 'GUILD_TEXT'
+            );
 
-              if (globalChatChannel) {
-                const codeBlock = "\n" + message.content + "";
-                const combinedMessage = `${codeBlock}\n[Invite me](https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&permissions=8&scope=bot)・[Support](https://discord.gg/YOUR_DISCORD_SERVER)`;
-                const embed = new MessageEmbed()
-                  .setColor('#0099ff')
-                  .setAuthor(`${message.guild.name} - ${message.author.tag}`, message.guild.iconURL())
-                  .setDescription(combinedMessage);
+            if (globalChatChannel) {
+              const cleanedContent = message.cleanContent; 
+              const combinedMessage = `${cleanedContent}\n[Invite me](https://discord.com/api/oauth2/authorize?client_id=1141889300330455080&permissions=8&scope=bot)・[Support](https://discord.gg/5cCAVMV6Me)`;
+              const embed = new MessageEmbed()
+                .setColor('#3498db')
+                .setAuthor(`${message.guild.name} - ${message.author.tag}`, message.guild.iconURL())
+                .setDescription(`**Nachricht von ${message.author.tag}:**\n${cleanedContent}`)
+                .setFooter(`User ID: ${message.author.id}`);
+                
 
-                globalChatChannel.send({ embeds: [embed] });
+              if (message.attachments.size > 0) {
+                const attachment = message.attachments.first();
+                embed.setImage(attachment.url);
               }
+
+              if (message.stickers.size > 0) {
+                const sticker = message.stickers.first();
+                embed.setImage(sticker.url);
+              }
+
+              globalChatChannel.send({ embeds: [embed] });
             }
-          });
-        }
+          }
+        });
       }
     }
   }
@@ -76,29 +137,10 @@ client.on('messageCreate', async message => {
 
 client.on('messageCreate', message => {
   if (message.content.startsWith(`${prefix}help`)) {
-    message.reply('Der Kanal muss den Namen "global chat" (unabhängig von anderen Wörtern im Namen) enthalten, damit der Bot funktioniert.');
+    message.reply('**Bot dont Have Commands**In order for the bot to work, you need a channel that has a global name, the messages will be sent to every server on which the bot is in the channel with a global name. If problems occur, please come to the support server immediately and open a ticket');
   }
 });
 
-const blockedUsers = new Map();
 
-client.on('messageCreate', async message => {
-  if (message.content.startsWith(`${prefix}block`)) {
-    const args = message.content.split(' ');
-    if (args.length >= 3) {
-      const targetUser = message.mentions.users.first();
-      if (targetUser) {
-        const reason = args.slice(2).join(' ');
-        blockedUsers.set(targetUser.id, reason);
-        message.reply(`Du hast ${targetUser.tag} erfolgreich blockiert. Grund: ${reason}`);
-      } else {
-        message.reply('Du musst einen Benutzer mit "@" erwähnen.');
-      }
-    } else {
-      message.reply('Falsche Verwendung des Befehls. Verwende: !block @username grund');
-    }
-  }
-});
-
-const token = 'DEIN_BOT_TOKEN';
+const token = '';
 client.login(token);
